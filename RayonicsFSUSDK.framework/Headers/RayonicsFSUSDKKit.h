2185d070af8c8e942042610a030b460cc75c716c
Piccolo <popking5466@gmail.com>
2018-05-02 17:44:03 +0800

    rm copy h files

---
+++ RayonicsFSUSDK.framework/Headers/RayonicsFSUSDKKit.h
@@ -0,0 +1,39 @@
+//
+//  RayonicsFSUSDKKit.h
+//  RayonicsFSUSDK
+//
+//  Created by Piccolo on 2018/5/2.
+//  Copyright © 2018 Piccolo. All rights reserved.
+//
+
+#ifndef RayonicsFSUSDKKit_h
+#define RayonicsFSUSDKKit_h
+
+#import <RayonicsFSUSDK/RAFSUSDKActiveReportInfo.h>
+#import <RayonicsFSUSDK/RAFSUSDKBase.h>
+#import <RayonicsFSUSDK/RAFSUSDKBaseDelegate.h>
+#import <RayonicsFSUSDK/RAFSUSDKDetectInfo.h>
+#import <RayonicsFSUSDK/RAFSUSDKDoActionInfo.h>
+#import <RayonicsFSUSDK/RAFSUSDKDoParameterInfo.h>
+#import <RayonicsFSUSDK/RAFSUSDKEthernetParameterInfo.h>
+#import <RayonicsFSUSDK/RAFSUSDKHeartBeatInfo.h>
+#import <RayonicsFSUSDK/RAFSUSDKLockInfo.h>
+#import <RayonicsFSUSDK/RAFSUSDKMotorActionInfo.h>
+#import <RayonicsFSUSDK/RAFSUSDKOnlineOpenInfo.h>
+#import <RayonicsFSUSDK/RAFSUSDKPeripheral.h>
+#import <RayonicsFSUSDK/RAFSUSDKPPPAddressInfo.h>
+#import <RayonicsFSUSDK/RAFSUSDKPublicCommon.h>
+#import <RayonicsFSUSDK/RAFSUSDKPublicDefine.h>
+#import <RayonicsFSUSDK/RAFSUSDKPublicTypeEnum.h>
+#import <RayonicsFSUSDK/RAFSUSDKResultInfo.h>
+#import <RayonicsFSUSDK/RAFSUSDKServiceInfo.h>
+#import <RayonicsFSUSDK/RAFSUSDKSimCardInfo.h>
+#import <RayonicsFSUSDK/RAFSUSDKSystemUpdateInfo.h>
+#import <RayonicsFSUSDK/RAFSUSDKThresholdInfo.h>
+#import <RayonicsFSUSDK/RAFSUSDKTimeInfo.h>
+#import <RayonicsFSUSDK/RAFSUSDKWarningIntervalInfo.h>
+#import <RayonicsFSUSDK/RAFSUSDKWirelessInfo.h>
+#import <RayonicsFSUSDK/RAGeneralFSUSDK.h>
+#import <RayonicsFSUSDK/RASimpleFSUSDK.h>
+
+#endif /* RayonicsFSUSDKKit_h */
